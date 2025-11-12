3470528552bcf54f91e194ffbdabf057401a3a23
Dinesh Singh <dineshamravatisingh@gmail.com>
2025-11-12 16:35:06 +0530

    Navbar and Footer Added

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from datetime import datetime
+from flask import Flask,render_template,redirect
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello World!"
+    year = datetime.now().year
+    return render_template('home.html',year=year)
 
 if __name__ == '__main__':
     app.run(debug=True)
